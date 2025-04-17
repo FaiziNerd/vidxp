12af365d34acc6bb7f2f90609deb1ef094f3dfc3
Muhammad Haroon <haroongondal347@gmail.com>
2025-04-17 16:57:45 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,59 +1,59 @@
-import whisperx
-import torch
-import clip
 import cv2
-from sentence_transformers import SentenceTransformer
-from moviepy.editor import VideoFileClip
-import chromadb
+import clip
 import typer
-import gc
+import torch
+import chromadb
+import whisperx
 from PIL import Image
+from rich import print
+from moviepy.editor import VideoFileClip
+from sentence_transformers import SentenceTransformer
 
 app = typer.Typer()
 
-embedder = SentenceTransformer("all-MiniLM-L6-v2")
-chroma_client = chromadb.PersistentClient(path="./chroma_data")
 device = "cpu"
+embedder  = SentenceTransformer(r"./models--sentence-transformers--all-MiniLM-L6-v2/snapshots/c9745ed1d9f207416be6d2e6f8de32d1f16199bf")
 clip_model, preprocess = clip.load("ViT-B/32", device=device)
+chroma_client = chromadb.PersistentClient(path="./chroma_data")
 voice_collection = chroma_client.get_or_create_collection(name="voiceEmbeddings")
 scene_collection = chroma_client.get_or_create_collection(name="sceneEmbeddings")
 
-
+@app.command()
 def videoindex(path: str):
-    print("Audio Indexing...")
+
+    video = VideoFileClip(path)
+    video_audio = video.audio
+    audio = "audio.wav"
+    video_audio.write_audiofile(audio)
+
+    print("[bold red]Video Indexing...[/bold red]")
+
+    print("[green]Audio Indexing...[/green]")
 
     batch_size = 16
     compute_type = "float32"
 
-    filepath = path
-
-    video = VideoFileClip(filepath)
-    movie_audio = video.audio
-    audio = "audio.wav"
-    movie_audio.write_audiofile(audio)
-
-    whisper_model = whisperx.load_model("large-v2", device, compute_type=compute_type)
+    whisper_model = whisperx.load_model(r"./models/models--Systran--faster-whisper-large-v2/snapshots/f0fe81560cb8b68660e564f55dd99207059c092e", device, compute_type=compute_type)
 
     audio = whisperx.load_audio(audio)
     result = whisper_model.transcribe(audio, batch_size=batch_size, language="en")
 
-    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device, model_dir="./torch")
     result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
 
     segments = result["segments"]
 
-    id = 0
+    id = 0 
     for segment in segments:
         dialogue = segment["text"]
         dialogue_embedding = embedder.encode(dialogue, convert_to_tensor=True)
-        voice_collection.add(ids=[f"{id}"], embeddings=[dialogue_embedding.tolist()],
-                             metadatas=[{"start": segment["start"]}])
-        id += 1
+        voice_collection.add(ids=[f"{id}"],embeddings=[dialogue_embedding.tolist()], metadatas=[{"start":segment["start"]}])
+        id+=1
+    
+    print("[green]Audio Indexing Complete !!![/green]")
 
-    print("Audio Indexing Complete !!!")
-
-    print("Scene Indexing...")
-
+    print("[green]Scene Indexing...[/green]")
+    
     id = 0
     time = 0.0
 
@@ -81,48 +81,47 @@ def videoindex(path: str):
         time += frame_time
 
     video.release()
-    print("Scene Indexing Complete !!!")
 
+    print("[green]Scene Indexing Complete !!![/green]")
 
-def dialogue(path: str, dialogue: str):
-    filepath = path
+    print("[bold red]Video Indexing Complete !!![/bold red]")
+
+@app.command()
+def dialogue(dialogue: str):
+
+    print("[green]Searching dialogue...[/green]")
 
     query = dialogue
     query_embedding = embedder.encode(query, convert_to_tensor=True)
 
     result = voice_collection.query(query_embeddings=[query_embedding.tolist()], include=["metadatas"], n_results=1)
+    time = result["metadatas"][0][0]["start"]
 
-    if result["metadatas"]:
-        time = result["metadatas"][0][0]["start"]
-    else:
-        print("No matching dialogue found.")
-        return None
+    print("[green]Dialogue found !!![/green]")
 
     return time
 
+@app.command()
+def scene(scene: str):
 
-def scene(path: str, scene: str):
-    filepath = path
+    print("[green]Searching scene...[/green]")
 
     query = scene
     query = clip.tokenize([query]).to(device)
-
+ 
     with torch.no_grad():
         query_features = clip_model.encode_text(query)
         query_features /= query_features.norm(dim=-1, keepdim=True)
-
+     
     query_embedding = query_features.cpu().numpy().tolist()[0]
+ 
+    result = scene_collection.query(query_embeddings=[query_embedding],include=["metadatas"],n_results=1)
+ 
+    time = result["metadatas"][0][0]["time"]
 
-    result = scene_collection.query(query_embeddings=[query_embedding], include=["metadatas"], n_results=1)
-
-    if result["metadatas"]:
-        time = result["metadatas"][0][0]["time"]
-    else:
-        print("No matching scene found.")
-        return None
+    print("[green]Scene found...[/green]")
 
     return time
 
-
-if _name_ == "_main_":
-    app() 
+if __name__ == "__main__":
+    app()
